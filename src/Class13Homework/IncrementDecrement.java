d8a0db77aedc0af00416151dc4aba1a2072bbe7c
Radha solanki <rps3308@gmail.com>
2021-10-20 20:13:41 +0100

    commit

---
+++ src/Class13Homework/IncrementDecrement.java
@@ -0,0 +1,19 @@
+package Class13Homework;
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+
+
+        int a, b;
+
+        a = 10;
+        System.out.println(a);
+        a++;
+
+
+
+    }
+
+
+}

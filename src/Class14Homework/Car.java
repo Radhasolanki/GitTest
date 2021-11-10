1b6eb078b20283aa62219474a3a92440623487ae
Radha solanki <rps3308@gmail.com>
2021-11-10 13:44:37 +0000

    commit

---
+++ src/Class14Homework/Car.java
@@ -0,0 +1,46 @@
+package Class14Homework;
+
+  // object concept
+
+public class Car {
+
+  int mod;
+  int wheel;
+
+
+    // Car = is class name
+  // new = is used to create the object
+  // a = is object reference variables
+    
+    
+    public static void main (String[] args) {
+
+        Car a = new Car();
+        Car b = new Car();
+        Car c = new Car();
+
+        a.mod = 2015;
+        a.wheel = 4;
+
+
+
+        System.out.println(a.mod);
+        System.out.println(a.wheel);
+
+    }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+
+    
+
